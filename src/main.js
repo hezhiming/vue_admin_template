16c8991ac653584a84cb8f136d13f6bd19db3369
he.zhiming <he-zhiming@foxmail.com>
2020-03-01 14:39:57 +0800

    路由文件,添加一些注释,方便看懂

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import '@/styles/index.scss' // global css
 
 import App from './App'
 import store from './store'
-import router from './router'
+import ROUTER from './router'
 
 import './icons' // icon
 import './permission' // permission control
@@ -45,7 +45,7 @@ Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
-  router,
+  router: ROUTER,
   store,
   render: h => h(App)
 })
